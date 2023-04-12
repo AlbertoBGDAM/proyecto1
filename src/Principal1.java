f4ed3e502b40bbba0a6e6b76e578ccee2f6f83e3
AlbertoBGDAM <alberto.bargut@educa.jcyl.es>
2023-04-12 14:05:46 +0200

    segundo commit

--- src/Principal1.java
+++ src/Principal1.java
@@ -4,6 +4,8 @@ public class Principal1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hola git");
+		System.out.print("Hola git");
+		System.out.print("Hola git");
 	}
 
 }
