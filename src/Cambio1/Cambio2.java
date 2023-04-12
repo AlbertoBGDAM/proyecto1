8cca554320a0da5e2b76f74996524d7de8dabf7b
AlbertoBGDAM <alberto.bargut@educa.jcyl.es>
2023-04-12 18:27:58 +0200

    commit Cambio2 Eclipseg1

--- src/Cambio1/Cambio2.java
+++ src/Cambio1/Cambio2.java
@@ -2,6 +2,6 @@ package Cambio1;
 
 public class Cambio2 {
 	public static void main(String[] args) {
-		System.out.print("Pruebas Git Clase Cambio2 CloneGit");
+		System.out.print("Pruebas Git Clase Cambio2 EclipseG1");
 		}
 }
